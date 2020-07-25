32dc7aab79cffb47e50039d09fdf162566933421
ZhaErzzzzz <1351310082@qq.com>
2020-07-25 10:22:01 +0800

    HelloWorld

---
+++ src/com/company/HelloWorld/Main.java
@@ -0,0 +1,9 @@
+package com.company.HelloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("sasa");
+        System.out.println("nihao ");
+            }
+}
